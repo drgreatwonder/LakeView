e4b651a2bb0666b66c4ff33b92e94fc25239b1b6
Chidinma <muoghaluchidinma@gmail.com>
2020-02-20 12:05:36 +0100

    landlord registration page completed

--- landlordreg.php
+++ landlordreg.php
@@ -1,35 +1,33 @@
-<?php require "header.php" ?>
+<?php require "header.php"?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
- 
+
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
   </head>
 
   <body class="getbody p-5 m-5">
-        <div class="container p-5">
-          <h1 class="text-center mb-3">REGISTER</h1>
       
+          <h1 class="text-center mb-3">REGISTER</h1>
+
           <form name="getstarted" action="" method="post" enctype="multipart/form-data">
 
-            <fieldset  class="p-3 mt-5"> 
+            <fieldset  class="p-3 mt-5">
               <legend>Personal Information</legend>
-            <div class="row">
-      
-            </div>
+          
             <div class="row mb-4">
               <div class="col-md-6 col-sm-12">
                 <label>First Name</label>
                 <input type="text" class="form-control" name="firstname" placeholder="Enter First Name" required/>
               </div>
-      
+
               <div class="col-md-6 col-sm-12">
                 <label>Last Name</label>
                 <input type="text" class="form-control" name="lastname" placeholder="Enter Last Name" required/>
               </div>
             </div>
-      
+
             <div class="row mb-4">
               <div class="col-md-6 col-sm-12">
                 <label>Username</label>
@@ -40,7 +38,7 @@
                 <input type="date" class="form-control" name="dob" required/>
               </div>
             </div>
-      
+
             <div class="row mb-4">
               <div class="col-md-6 col-sm-12">
                 <label>Email address</label>
@@ -51,8 +49,8 @@
                 <input type="email" class="form-control" name="email1" placeholder="Enter email" required/>
               </div>
             </div>
-      
-      
+
+
             <div class="row mb-4">
               <div class="col-md-6 col-sm-12">
                 <label>Password</label>
@@ -63,7 +61,7 @@
                 <input type="password" class="form-control" name="password1" placeholder="Confirm Password" required/>
               </div>
             </div>
-      
+
             <div class="row mb-3">
               <div class="col-md-6 col-sm-12">
                 <label>Gender</label>
@@ -89,46 +87,55 @@
               </div>
             </div>
           </fieldset>
+          </form>
 
 
 
+          <form name="getstarted" action="" method="post" enctype="multipart/form-data">
 
-          <fieldset class="p-3 mt-5"> 
-            <legend>Property Information</legend>
           
-          
-            <div class="m-3">
+                      <fieldset  class="p-3 mt-5">
+                        <legend>Property Information</legend>
+                        
+      <div class="row">
+
+            <div class="col-md-6 col-sm-12">
+
+            <div class="mb-4">
               <label>Property Name</label>
-              <input type="text" class="form-control w-50" name="propertyname" placeholder="Enter First Name" required/>
-            </div>
-    
-            <div class="m-3">
+              <input type="text" class="form-control" name="propertyname" placeholder="Enter Property Name" required/>
+              </div>
+
+              <div class="mb-4">
               <label>Address</label>
-              <input type="text" class="form-control w-50" name="lastname" placeholder="Enter Last Name" required/>
+              <input type="text" class="form-control" name="lastname" placeholder="Enter Address" required/>
             </div>
-         
-      
-            <div class="m-3">
+
+            <div class="mb-4">
               <label>Year Built</label>
-              <input type="date" class="form-control w-50" name="dob" required/>
+              <input type="date" class="form-control" name="dob" required/>
             </div>
-         
-            <div class="m-3">
-              <label>Property Type</label>
-              <select class="form-control w-50" name="most_preferred_language" required>
+
+            <div class="mb-4">
+                <label>Property Type</label>
+              <select class="form-control" name="most_preferred_language" required>
                 <option>Take your pick</option>
                 <option>Personal</option>
                 <option>Commercial</option>
                 <option>Undeveloped</option>
-              
               </select>
-              <div class="form-group mt-3">
-                <label>Upload Profile Picture</label>
+            </div>
+
+              <label>Upload Profile Picture</label>
                 <input type="file" class="form-control-file" name="profile_pix">
-              </div>
+
+          </div>
+
+      </div>
+
     
-        </fieldset>
-        
-        <button type="submit" class="btn btn-primary text-secondary" name="getstarted" onclick="showLoginModal()">Submit</button>
+        <button type="submit" class="btn btn-primary text-secondary mt-5" name="getstarted" onclick="showLoginModal()">Submit</button>
+    
           </form>
-  
+  </body>
+</html>
